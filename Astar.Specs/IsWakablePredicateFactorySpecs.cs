00bd1283036a9c728993d281fbf8ba4f2071fe7b
JorgeGamba <jorgegamba@gmail.com>
2017-08-26 19:39:41 +0000

    Simply creates the function to check if a point is walkable or not

---
+++ Astar.Specs/IsWakablePredicateFactorySpecs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Doing.BDDExtensions;
+using FluentAssertions;
+using NUnit.Framework;
+using static Astar.IsWakablePredicateFactory;
+
+namespace Astar.Specs
+{
+    [Category("IsWakablePredicateFactory")]
+    public class IsWakablePredicateFactorySpecs : FeatureSpecifications
+    {
+        public override void When() =>
+            _result = _predicate(_point);
+
+        public class When_the_provided_point_is_not_recognized : IsWakablePredicateFactorySpecs
+        {
+            public override void Given()
+            {
+                _predicate = CreateIsWakablePredicate(new HashSet<Point>());
+                _point = _somePoint;
+            }
+
+            [Test]
+            public void Should_indicate_that_it_is_not_walkable() =>
+                _result.Should().BeFalse();
+        }
+
+        public class When_the_provided_point_is_recognized : IsWakablePredicateFactorySpecs
+        {
+            public override void Given()
+            {
+                _predicate = CreateIsWakablePredicate(new HashSet<Point> { _somePoint});
+                _point = _somePoint;
+            }
+
+            [Test]
+            public void Should_indicate_that_it_is_walkable() =>
+                _result.Should().BeTrue();
+        }
+
+
+        Predicate<Point> _predicate;
+        Point _point;
+        bool _result;
+
+        static Point _somePoint = new Point(10, 10);
+    }
+}
---
+++ Astar/IsWakablePredicateFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Astar
+{
+    public static class IsWakablePredicateFactory
+    {
+        public static Predicate<Point> CreateIsWakablePredicate(HashSet<Point> walkablePoints) => 
+            walkablePoints.Contains;
+    }
+}
